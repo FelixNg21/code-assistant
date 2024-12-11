50588e304559605acf98010fcd5030f93ffd047e
GitHub Action <action@github.com>
2024-12-11 19:05:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/felixng21/codeassistant/MyBundle.kt
+++ src/main/kotlin/com/github/felixng21/codeassistant/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.felixng21.codeassistant
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/felixng21/codeassistant/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/felixng21/codeassistant/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.felixng21.codeassistant.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
